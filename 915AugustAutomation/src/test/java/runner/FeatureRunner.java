b3721cf9fd05dd5df9fd13c707dfda5a72129d8c
LearnMoreTraining <108234734+LearnMoreTraining@users.noreply.github.com>
2022-10-13 18:33:09 +0530

    Update FeatureRunner.java

--- 915AugustAutomation/src/test/java/runner/FeatureRunner.java
+++ 915AugustAutomation/src/test/java/runner/FeatureRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 
-
+test
 @CucumberOptions(
 
         features = {"src/test/resources/feature/SaleforceErrorMessageValidation.feature"},
